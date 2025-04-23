2bdcc63b9cd2e6bf83804e6a6d319750e9971ac2
Alex Braun <alexander.g.braun@gmail.com>
2025-04-23 04:19:12 +0000

    0.6.2

--- python/lunchbox/stopwatch_test.py
+++ python/lunchbox/stopwatch_test.py
@@ -12,12 +12,12 @@ class StopwatchTests(unittest.TestCase):
         time.sleep(0.01)
         stopwatch.stop()
 
-        self.assertAlmostEqual(stopwatch.delta.microseconds, 10000, delta=10000)
+        self.assertAlmostEqual(stopwatch.delta.microseconds, 10000, delta=100000)
         self.assertEqual(stopwatch.human_readable_delta, '0.01 seconds')
 
         stopwatch.start()
         time.sleep(0.02)
         stopwatch.stop()
 
-        self.assertAlmostEqual(stopwatch.delta.microseconds, 20000, delta=10000)
+        self.assertAlmostEqual(stopwatch.delta.microseconds, 20000, delta=100000)
         self.assertEqual(stopwatch.human_readable_delta, '0.02 seconds')
